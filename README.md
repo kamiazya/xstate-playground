4ae1f83bffe8eac6ea734ffcb5180398c2c705ae
Yuki Yamazaki <35218186+kamiazya@users.noreply.github.com>
2022-01-13 16:34:10 +0000

    add promise state machine

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 
 ```bash
 $ yarn start counter
-CONTEXT { count: 0 } [ 'INC', 'DEC' ]
+active { count: 0 } [ 'INC', 'DEC' ]
 INC
-CONTEXT { count: 1 } [ 'INC', 'DEC' ]
+active { count: 1 } [ 'INC', 'DEC' ]
 DEC
-CONTEXT { count: 0 } [ 'INC', 'DEC' ]
+active { count: 0 } [ 'INC', 'DEC' ]
+INC
+active { count: 1 } [ 'INC', 'DEC' ]
+INC
+active { count: 2 } [ 'INC', 'DEC' ]
+^C
+$ yarn start promise
+pending undefined [ 'RESOLVE', 'REJECT' ]
+RESOLVE
+resolved undefined []
+<DONE>
 ```
--- src/cli.ts
+++ src/cli.ts
@@ -15,10 +15,14 @@ async function main(name: string) {
   const machine: StateMachine<any, any, any> = (await import(`./${name}`)).default;
 
   const service = interpret(machine)
-    .onTransition((state) => console.log('CONTEXT', state.context, state.nextEvents))
+    .onTransition((state) => console.log(state.value, state.context, state.nextEvents))
     .start();
   reader.on('line', (input) => {
-    service.send(input);
+    const state = service.send(input);
+    if (state.done) {
+      console.log('<DONE>');
+      reader.close();
+    }
   });
 }
 
